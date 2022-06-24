b063fda5731397414c32adb141a283793a82f2e1
guilhermelevi <guilhermelimeira2003@gmail.com>
2022-06-24 09:06:35 -0300

    init video component

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,3 +1,45 @@
-export const Video = () => {
-    return <h1>Videos</h1>
+import { gql, useQuery } from "@apollo/client"
+import { FC } from "react"
+
+const GET_LESSONS_QUERY = (id:string) => gql`
+    query MyQuery {
+        lessons(orderBy: availableAt_ASC, where: id == ${id}) {
+            id
+            slug
+            title
+            description
+            teacher {
+                name
+            }
+        }
+    }
+`
+
+interface VideoProps {
+    id: string
+}
+
+interface LessonProps {
+    id: string
+    title: string
+    description: string
+    teacher: {
+        name: string
+    }
+}
+
+export const Video:FC<VideoProps> = ({id}) => {
+    const { data } = useQuery<{ lessons: LessonProps }>(GET_LESSONS_QUERY(id))
+    
+    return (
+        <div>
+            <video src="">video</video>
+            <h1>{data?.lessons.title}</h1>
+            <span>{data?.lessons.description}</span>
+            <div>
+                <img src={data?.lessons.teacher.imgUrl} alt="" />
+                <span>{data?.lessons.teacher.name}</span>
+            </div>
+        </div>
+    )
 }
